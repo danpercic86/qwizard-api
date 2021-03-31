081b51976b8fc4349916a01988c628a3ad9f594a
Dan Percic <danpercic86@gmail.com>
2021-03-31 16:43:02 +0300

    add fizzbuzz game

--- example.rb
+++ example.rb
@@ -1,10 +1,52 @@
-puts 'This is the first executed statement'
+class FizzBuzz
+  @@fizz_count = 0
+  @@buzz_count = 0
+  @n = 0
 
-# I am a comment
-a = 1 + (2 * 3)
+  def initialize(n)
+    @n = n
+  end
 
-if a == 6
-  puts 'Will I be printed?!'
+  def fizz_count
+    @@fizz_count
+  end
+
+  def buzz_count
+    @@buzz_count
+  end
+
+  def play
+    i = 1
+    while i <= @n
+      if i % 3 == 0 && i % 5 == 0
+        puts 'fizzbuzz'
+      elsif i % 3 == 0
+        puts 'fizz'
+        @@fizz_count = @@fizz_count + 1
+      elsif i % 5 == 0
+        puts 'buzz'
+        @@buzz_count = @@buzz_count + 1
+      else
+        puts i
+      end
+      i = i + 1
+    end
+  end
+
+  def reset_count
+    @@fizz_count = 0
+    @@buzz_count = 0
+  end
 end
 
-puts 'The end!'
+fizzbuzz = FizzBuzz.new(16)
+fizzbuzz.play
+
+fizzbuzz2 = FizzBuzz.new(32)
+fizzbuzz2.play
+
+puts 'fizz count:'
+puts  fizzbuzz.fizz_count
+
+puts 'buzz count:'
+puts  fizzbuzz.buzz_count
