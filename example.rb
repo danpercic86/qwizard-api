d34b85ca2e034dc87f48c93a0d69499e5b253e95
Dan Percic <danpercic86@gmail.com>
2021-03-31 18:29:59 +0300

    update fizzbuzz

--- example.rb
+++ example.rb
@@ -1,41 +1,61 @@
 class FizzBuzz
-  @@fizz_count = 0
-  @@buzz_count = 0
-  @n = 0
-
-  def initialize(n)
-    @n = n
+  @number = 0
+  # attr_reader :buzz_count
+  # attr_writer :buzz_count
+  class << self
+    attr_accessor :buzz_count, :fizz_count
   end
 
-  def fizz_count
-    @@fizz_count
+  def initialize(number)
+    @number = number
   end
 
-  def buzz_count
-    @@buzz_count
-  end
-
+  # rubocop:disable Metrics/MethodLength
   def play
     i = 1
-    while i <= @n
-      if i % 3 == 0 && i % 5 == 0
+    while i <= @number
+      if (i % 3).zero? && (i % 5).zero?
         puts 'fizzbuzz'
-      elsif i % 3 == 0
+        FizzBuzz.increment
+      elsif (i % 3).zero?
         puts 'fizz'
-        @@fizz_count = @@fizz_count + 1
-      elsif i % 5 == 0
+        FizzBuzz.increment_fizz
+      elsif (i % 5).zero?
         puts 'buzz'
-        @@buzz_count = @@buzz_count + 1
+        FizzBuzz.increment_buzz
       else
         puts i
       end
-      i = i + 1
+      i += 1
     end
   end
 
-  def reset_count
-    @@fizz_count = 0
-    @@buzz_count = 0
+  # rubocop:enable Metrics/MethodLength
+
+  def self.increment_buzz
+    FizzBuzz.buzz_count = (FizzBuzz.buzz_count || 0) + 1
+  end
+
+  def self.increment_fizz
+    FizzBuzz.fizz_count = (FizzBuzz.fizz_count || 0) + 1
+  end
+
+  def self.increment
+    FizzBuzz.increment_buzz
+    FizzBuzz.increment_fizz
+  end
+
+  # def self.buzz_count
+  #   @buzz_count
+  # end
+  #
+  # def self.buzz_count=(val)
+  #   @buzz_count = val
+  # end
+
+  def self.reset_count
+    FizzBuzz.fizz_count = 0
+    FizzBuzz.buzz_count = 0
   end
 end
 
@@ -46,7 +66,7 @@ fizzbuzz2 = FizzBuzz.new(32)
 fizzbuzz2.play
 
 puts 'fizz count:'
-puts  fizzbuzz.fizz_count
+puts FizzBuzz.fizz_count
 
 puts 'buzz count:'
-puts  fizzbuzz.buzz_count
+puts FizzBuzz.buzz_count
